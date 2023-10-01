58d08c0fa9d1b3ee6850baf86ea1fc08a958db09
Harsh <singhharsh8283@gmail.com>
2023-10-01 12:52:50 +0530

    reduxconfig

---
+++ src/app/store.js
@@ -0,0 +1,16 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+
+import userSlice from "../features/userSlice";
+
+//persis 
+const rootReducer = combineReducers({
+
+    user:userSlice,
+});
+
+export const store = configureStore(
+    {
+  reducer : rootReducer,
+  devTools : true,
+    }
+);
