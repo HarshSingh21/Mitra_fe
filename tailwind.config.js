61700f14cce2aea97bee25bf3aa96c0c48ed6f03
Harsh Singh <singhharsh8283@gmail.com>
2023-11-19 21:33:36 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-
-
-  
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: "class",
   // card // add link // symbol// card layout// add unit to chat
